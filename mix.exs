50a6f9647badf570d815fdc054253d96d5ed7be7
Yury Gargay <yury.gargay@gmail.com>
2015-07-22 18:33:31 +0600

    Update exometer

--- mix.exs
+++ mix.exs
@@ -17,9 +17,8 @@ defmodule Metricman.Mixfile do
   defp deps(_) do
     [
      {:lager, "~> 2.1.1", override: true},
-     {:exometer, github: "Feuerlabs/exometer"},
      {:meck, "~> 0.8.2", override: true},
-     {:exometer_core, github: "Feuerlabs/exometer_core", branch: "master", override: true},
+     {:exometer, github: "xerions/exometer", branch: "master", override: true},
      {:edown, github: "uwiger/edown", branch: "master", override: true},
      {:mock, github: "jjh42/mock"}]
   end
