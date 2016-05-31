ea272b4c63021907c0d442e55af8d7b022570067
Yury Gargay <yury.gargay@gmail.com>
2016-05-31 12:52:16 +0300

    Version 1.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Metricman.Mixfile do
 
   def project do
     [app: :metricman,
-     version: "1.3.1",
+     version: "1.4.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
