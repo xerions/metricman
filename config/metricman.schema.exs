d405f46a6aa4ff770bc5127e033d3aa38229b08c
Alexander Kuleshov <kuleshovmail@gmail.com>
2016-07-13 13:44:06 +0600

    use upstream conform compatible schema

--- config/metricman.schema.exs
+++ config/metricman.schema.exs
@@ -4,78 +4,73 @@
       doc: """
       Influxdb configuration in form of <shema>://<host>[:<port>]/<database>
       """,
-      to: "exometer_core.report.reporters",
+      to: "exometer_core.report.reporters.exometer_report_influxdb.db",
       datatype: :binary,
-      default: false
+      default: "false"
     ],
     "metrics.influx.tags": [
       doc: """
       Influxdb additional tags for each metric in form of <key>:<value>,...
       """,
-      to: "exometer_core.report.reporters",
+      to: "exometer_core.report.reporters.exometer_report_influxdb.tags",
       datatype: [list: :binary],
-      default: ""
+      default: []
     ],
     "metrics.influx.batch_window_size": [
       doc: """
       Set window size in ms for batch sending.
       This means reported will collect measurements within this interval and send all measurements in one packet.
       """,
-      to: "exometer_core.report.reporters",
+      to: "exometer_core.report.reporters.exometer_report_influxdb.batch_window_size",
       datatype: :integer,
       default: 0
     ]
   ],
-  translations: [
-    "metrics.influx.db": fn
-      _mapping, "false", nil -> nil
-      _mapping, "false", acc ->
-        Keyword.drop(acc, [:exometer_report_influxdb])
-      _mapping, db, acc ->
-        case URI.parse(db) do
-          %URI{scheme: protocol, host: host, port: port} = uri ->
-            db = case protocol do
-              "udp" -> []
-              http when http in ["http", "https"] -> 
-                "/" <> path = uri.path  
-                [db: path]
+  transforms: [
+    "exometer_core.report.reporters.exometer_report_influxdb": fn table ->
+      db = case Conform.Conf.get(table, "exometer_core.report.reporters.exometer_report_influxdb.db") do
+             [{_, db}] when is_binary(db) and db != "false" ->
+               case URI.parse(db) do
+                 %URI{scheme: protocol, host: host, port: port} = uri ->
+                   db = case protocol do
+                          "udp" -> []
+                          http when http in ["http", "https"] ->
+                            "/" <> path = uri.path
+                            [db: path]
+                          _ ->
+                             exit("Unsupported URI for InfluxDB: #{db}")
+                        end
+                   [protocol: protocol |> String.to_atom, host: host, port: port] ++ db
+               end
+             _ ->
+               [db: "false"]
+           end
+      tags = case Conform.Conf.get(table, "exometer_core.report.reporters.exometer_report_influxdb.tags") do
+               [{_, []}] ->
+                 []
+               [{_, tags}] ->
+                 tags = for tag <- tags do
+                   case String.split(tag, ":") do
+                     [key, value] -> {key |> String.to_atom, value}
+                     _ -> exit("Unsupported tags for InfluxDB: #{inspect tags}")
+
+                   end
+                 end
+                 [tags: tags]
             end
-            params = Access.get(acc || [], :exometer_report_influxdb, [])
-            Keyword.put(acc || [], :exometer_report_influxdb, params ++ [protocol: protocol |> String.to_atom, host: host, port: port] ++ db)
-          _ ->
-            exit("Unsupported URI for InfluxDB: #{db}")
-        end
-      _, db, _ ->
-        exit("Unsupported URI for InfluxDB: #{db}")
-    end,
-    "metrics.influx.tags": fn
-      _mapping, [""], acc -> acc
-      _mapping, _, nil -> nil
-      _mapping, tags, acc ->
-        tags = for tag <- tags do
-          case String.split(tag, ":") do
-            [key, value] -> {key |> String.to_atom, value}
-            _ ->
-              exit("Unsupported tags for InfluxDB: #{inspect tags}")
-          end
-        end
-        if Keyword.has_key?(acc, :exometer_report_influxdb) do
-          params = Access.get(acc, :exometer_report_influxdb, [])
-          Keyword.put(acc, :exometer_report_influxdb, params ++ [tags: tags])
-        else acc end
-      _, tags, _ ->
-        exit("Unsupported tags for InfluxDB: #{inspect tags}")
-    end,
-    "metrics.influx.batch_window_size": fn
-      _mapping, _, nil -> []
-      _mapping, window_size, acc ->
-        if not is_integer(window_size) do
-          exit("Unsupported batch_window_size for InfluxDB: #{inspect window_size}")
-        end
-        if Keyword.has_key?(acc, :exometer_report_influxdb) do
-          params = Access.get(acc, :exometer_report_influxdb)
-          Keyword.put(acc, :exometer_report_influxdb, params ++ [batch_window_size: window_size])
-        else acc end
-    end
+      batch_window_size = case Conform.Conf.get(table, "exometer_core.report.reporters.exometer_report_influxdb.batch_window_size") do
+                            [{_, []}] ->
+                              []
+                            [{_, window_size}] ->
+                              if not is_integer(window_size) do
+                                exit("Unsupported batch_window_size for InfluxDB: #{inspect window_size}")
+                              end
+                             [batch_window_size: window_size]
+                          end
+      :ets.delete(table, ['exometer_core', 'report', 'reporters', 'exometer_report_influxdb', 'db'])
+      :ets.delete(table, ['exometer_core', 'report', 'reporters', 'exometer_report_influxdb', 'tags'])
+      :ets.delete(table, ['exometer_core', 'report', 'reporters', 'exometer_report_influxdb', 'batch_window_size'])
+      db ++ tags ++ batch_window_size
+  end
   ]
 ]
